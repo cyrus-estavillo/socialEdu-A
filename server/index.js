06961593074c2f1283b29fb9db3946d6d31bfa24
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-09-21 11:59:55 -0400

    mongo db connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose"); 
+
+mongoose.connect('mongodb+srv://pranetallu:2LJEQQ8JE7EISp0s@cluster0.fskrd0v.mongodb.net/');
+
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error('Connection error:', error);
+});
+
+db.once('open', () => {
+  console.log('MongoDB connection successful');
+});
+
 
 app.listen(3001, () => {
     console.log("Server is on port 3001..")
-  })
+})
+
+
+  // pranetallu
+  // 2LJEQQ8JE7EISp0s
