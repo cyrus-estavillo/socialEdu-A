453ff23077e0dfb00e49dae80e9b2bc8e6dd5f6b
cyrus-estavillo <cyrus.estavillo@gmail.com>
2023-10-25 18:55:39 -0400

    Updaed landing page functionality

--- client/my-app/src/pages/Login.js
+++ client/my-app/src/pages/Login.js
@@ -1,15 +1,28 @@
+import React, { useState, useEffect, useContext } from 'react';
+import '../styling/Login.css'; 
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from "../components/UserContext";
 
 const Login = () => {
+    const [showButtons, setShowButtons] = useState(false);
+    const [formType, setFormType] = useState(null);
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const { setUserInfo } = useContext(UserContext);
 
+    const { setUserInfo } = useContext(UserContext);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        setTimeout(() => {
+        setShowButtons(true);
+        }, 3000);
+    }, []);
+
+    const handleButtonClick = (type) => {
+        setFormType(type);
+    };
 
     const login = async () => {
         const response = await fetch('http://localhost:3001/login', {
@@ -38,10 +51,26 @@ const Login = () => {
     }
 
     return (
-        <div style={{ marginTop: "10px" }}>
-            <TextField label="Username" variant="outlined" value={username} onChange={(e) => setUsername(e.target.value)} />
-            <TextField label="Password" variant="outlined" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <Button variant="contained" onClick={login}>Submit</Button>
+        <div className="login-container">
+          <img
+            src={`${process.env.PUBLIC_URL}/WISRR_Logo-Inverted.jpeg`}
+            alt="logo"
+            className={`logo ${showButtons ? 'slide-up' : ''}`}
+          />
+          {formType ? (
+            <div className="input-container">
+                <div className="white-input"> 
+                <TextField label="Username" variant="outlined"  value={username} onChange={(e) => setUsername(e.target.value)} />
+                <TextField label="Password" variant="outlined" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <Button variant="contained" onClick={login}>Submit</Button>
+            </div>
+          ) : (
+            <div className={`button-container ${showButtons ? 'visible' : ''}`}>
+              <Button variant="contained" onClick={() => handleButtonClick('login')}>Login</Button>
+              <Button variant="contained" onClick={() => handleButtonClick('signup')}>Sign Up</Button>
+            </div>
+          )}
         </div>
     )
 }
